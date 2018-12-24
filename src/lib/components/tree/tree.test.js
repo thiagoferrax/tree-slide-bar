7901c0d1459c0260a11c784e5b9baa9dba8bdc19
thiagoferrax <thiagoferrax@gmail.com>
2018-12-24 08:53:03 -0300

    Setting up unit testing with mocha and chai

---
+++ src/lib/components/tree/tree.test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect
+const Tree = require('./tree.jsx')
+
+describe('Tree', function() {
+    it('Should work!', function(){
+        expect(true).to.be.true
+    })
+})
+
