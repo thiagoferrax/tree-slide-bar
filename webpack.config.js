f514c92cdd368a5afa9b80c7d4bc14004c4b94bd
thiagoferrax <thiagoferrax@gmail.com>
2019-01-04 19:22:16 -0300

    Trying tofix the component.

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: path.resolve(__dirname, './src/lib/index.js'),
     output: {
       path: path.resolve(__dirname, './dist/lib'),
       filename: 'index.js',
+      library: ''
     },
     module: {
         rules: [
@@ -19,12 +20,11 @@ module.exports = {
             },
             {
                 test: /\.m?jsx?$/,
-                exclude: /(node_modules|bower_components)/,
                 use: {
                   loader: 'babel-loader',
                   options: {
                     presets: ['@babel/preset-env', '@babel/react'],
-th                    plugins: ['@babel/plugin-proposal-object-rest-spread']                    
+                    plugins: ['@babel/plugin-proposal-object-rest-spread']                    
                   }
                 }
             },
